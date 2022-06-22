75b0283c0c9f0356a1b5a019a4e2263cf493fc6b
Michael Landeros <michael.landeros@gmail.com>
2022-06-21 23:05:27 -0500

    Update endpoint uri

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-service.azurewebsites.net:$PORT/predict 
+     -X POST https://flask-ml-azure-serverless.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
